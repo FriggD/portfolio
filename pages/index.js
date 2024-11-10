2382567dc6846e58e929001c5f34deb56e566643
Frigg <glaucia.belo.dias@gmail.com>
2024-11-10 13:25:28 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home(){
-    return <h1>"1. Usar a ferramenta pra entregar um projeto até o final;"
-        "2. Que gere impacto em alguém;"
-        "3. Que seja uma experiencia prazerosa."
-        "Você não pode atravessar o mar simplesmente ficando parado e olhando para a água." R. Tagore</h1>
+    return <p>"1. Usar a ferramenta pra entregar um projeto até o final;"
+    "2. Que gere impacto em alguém;"
+    3. Que seja uma experiencia prazerosa."
+        <h1>"Você não pode atravessar o mar simplesmente ficando parado e olhando para a água." R. Tagore</h1></p>
 }
 
 export default Home;
